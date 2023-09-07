463366f776de959752db7c6d3f36a0e9fd91241d
CauanS29 <cauansantos043@gmail.com>
2023-09-07 16:33:17 -0300

    'criei a pasta com a nova tela de login e comecei uma parte'

---
+++ tela-login/my-app/src/routes/index.js
@@ -0,0 +1,24 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import Welcome from '../pages/Welcome'
+import SignIn from '../pages/SignIn'
+
+const stack = createNativeStackNavigator()
+
+export default function Routes(){
+    return(
+        <stack.Navigator>
+            <stack.Screen
+            name="Welcome"
+            component={Welcome}
+            options={{ headerShown: false}}
+        />
+
+        <stack.Screen
+            name="SignIn"
+            component={SignIn}
+            options={{ headerShown: false}}
+        />
+        </stack.Navigator>
+    )
+}
