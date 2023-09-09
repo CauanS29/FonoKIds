6e8b9d7855541ab93cc2e9720c5634d4d560c4fa
athena272 <guilhermera272@gmail.com>
2023-09-09 19:56:02 -0300

    Create index.js

---
+++ tela-login/my-app/src/pages/Progress/index.js
@@ -0,0 +1,37 @@
+
+
+import React from 'react' 
+import { 
+    View,
+    StyleSheet,
+    Image,
+    TouchableOpacity,
+    ImageBackground,
+    Text, 
+    TextInput} from 'react-native'  
+
+    import { useNavigation } from '@react-navigation/native'
+
+export default function Progress(){
+    const navigation = useNavigation();
+    
+    return (
+        <View styles={styles.container}>
+            <ImageBackground
+            source={require('../../assets/wallpaper.png')}
+            resizeMode='cover' 
+            style ={styles.image}
+            >
+
+            </ImageBackground>
+        </View>
+    )
+
+        
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+    },
+})
